e909d061eb0e2e9fa7b5958b3baaf0ffae23e0dc
MeetTandel <meettandel47@gmail.com>
2021-02-10 21:24:39 +0530

    All files

---
+++ index.js
@@ -0,0 +1,25 @@
+// DOM Manipulation
+const currentTimeDiv = document.querySelector("#screen");
+const currentDateDiv = document.querySelector("#date");
+
+// set current time
+function currentTime(){
+    let currTime = new Date().toLocaleTimeString('en-IT', {hour12: false});
+    currentTimeDiv.innerText = `${currTime}`;
+} 
+let increment = setInterval(currentTime, 1000);
+
+//start time when start button is pressed
+function startButton(){
+    increment = setInterval(currentTime, 1000);
+}
+
+//stop the time when stop button is pressed
+function stopButton(){
+    clearInterval(increment);
+}
+
+//display the date
+let today = new Date();
+let date = `${today.getDate()} / ${today.getMonth()} / ${today.getFullYear()}`;
+currentDateDiv.innerText = `${date}`;
